06319b267254413b995a47af8af395592b07eb0b
EileenMendoza18 <eileenjml4816@gmail.com>
2026-05-22 17:58:29 -0500

    feat: Se define la estructura inicial de la API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
 const express = require('express')
-const tasks = require('./routes/tasks.routes.js');
-const users = require('./routes/users.routes.js');
-
 const app = express()
 const port = 3000
 
@@ -9,5 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.use('/tasks', tasks);
-app.use('/users', users);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
